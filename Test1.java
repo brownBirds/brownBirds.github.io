8a0804a709770fd8b66bce5e7046f5689ddcb57e
wangyuhao <842441141@qq.com>
2021-07-14 20:24:32 +0800

    22

--- Test1.java
+++ Test1.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Author: WangYuhao
  * @Date:2021/5/23 15:59
  */
-public class Test {
+public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("11111");
