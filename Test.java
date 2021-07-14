a57065cb85edc9b4f0e6f480849b875c9a8c17e8
wangyuhao <842441141@qq.com>
2021-07-14 20:16:48 +0800

    111

--- Test.java
+++ Test.java
@@ -7,8 +7,6 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Test {
 
     public static void main(String[] args) {
-        ConcurrentHashMap<String, Integer> objectObjectConcurrentHashMap = new ConcurrentHashMap<>();
-        objectObjectConcurrentHashMap.put("1",1);
-        objectObjectConcurrentHashMap.put("1",2);
+        System.out.println("111");
     }
 }
